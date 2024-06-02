180743840a49d143f344cb06fbf2145b49169a9f
Eric L. Hepperle <codewizard13@gmail.com>
2024-06-02 05:31:20 -0500

    UPDATE scraper_help.js:  GET domain WORKS!

--- scraper_helper.js
+++ scraper_helper.js
@@ -31,8 +31,15 @@ const sel__link_rows = '.tab > div:nth-child(2)';
  */
 function main() {
     let linkRows = document.querySelectorAll(sel__link_rows);
-    let results = parseLinkRows(linkRows);
-    console.log(results);
+		
+		console.log(`linkRows Total = ${linkRows.length}`)
+
+    let resutls_obj = parseLinkRows(linkRows);
+    // console.log(resutls_obj.log_str);
+
+		// Launch in a new tab
+		// var new_tab = window.open('','TARGET')
+		// new_tab.document.body.innerHTML = resutls_obj.html_str
 }
 main();
 
@@ -41,15 +48,28 @@ main();
  * Parses the link rows and extracts relevant data.
  */
 function parseLinkRows(linkRows) {
-    let outStr = '';
+  
+	let outObj = {}
+	let log_str = ''
+	let html_str = ''
 
     for (let i = 0; i < linkRows.length; i++) {
         let row = linkRows[i];
+				let rowNum = i+1
 
         // Extracting the link
         let linkElement = row.querySelector('a.tabLink');
         let linkHref = linkElement ? linkElement.href : 'No link found';
         let linkText = linkElement ? linkElement.textContent : 'No link text found';
+
+				// Parse domain from link
+				if (linkHref !== 'No link found') {
+
+					let linkURL = new URL(linkHref)
+					let domain = linkURL.hostname
+					console.log(`Domain: ${domain}`)
+
+				}
 
         // Check for an image within the first div element
         let iconDiv = row.querySelector('div:first-child');
@@ -66,15 +86,52 @@ function parseLinkRows(linkRows) {
             }
         }
 
+				// BUILD LOG
+
         // Append extracted data to the output string
-        outStr += `Row ${i + 1}:\n`;
-        outStr += `Link Text: ${linkText}\n`;
-        outStr += `Link URL: ${linkHref}\n`;
-        outStr += `Icon Source: ${iconSrc}\n`;
-        outStr += '\n';
+        log_str += `Row ${i + 1}:\n`;
+        log_str += `Link Text: ${linkText}\n`;
+        log_str += `Link URL: ${linkHref}\n`;
+        log_str += `Icon Source: ${iconSrc}\n`;
+        log_str += '\n';
+
+				// BUILD DOM
+
+let row_htm = 				
+`<li class="row">
+	[Row #: ${rowNum}]:
+	<img
+		alt="favicon for "
+		class="row-icon"
+		src="chrome-extension://chphlpgkkbolifaimnlloiipkdnihall/_favicon/?pageUrl=https%3A%2F%2Fchatgpt.com%2Fc%2Feb76d760-9f75-402d-b42b-d1ae9dcbdf3c&amp;size=32"
+		\="" />
+	<a
+		href="https://chatgpt.com/c/eb76d760-9f75-402d-b42b-d1ae9dcbdf3c"
+		target="_blank">
+		Admin Bar Custom Count
+	</a>
+</li>
+<li class="row">
+	[Row #: 002]:
+	<img alt="favicon for " class="row-icon" src="" \="" />
+	<a
+		href="https://www.reddit.com/r/ProWordPress/comments/1d1qnhv/is_there_anything_like_local_wordpress_that/"
+		target="_blank">
+		Is there anything like Local Wordpress that doesn't lock you into a
+		specific host (ie Worpress Engine)? : r/ProWordPress
+	</a>
+</li>\n`
+
+			html_str += row_htm
+
+
     }
 
-    return outStr;
+		// Add data to outObj
+		outObj.log_str = log_str
+		outObj.html_str = html_str
+
+    return outObj;
 }
 
 
